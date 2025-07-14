82b3189adf7b4ff28d8f0eeae7517eae7a28c84d
Jonathan Lopez <jonathanglopez@gmail.com>
2025-07-14 11:21:22 -0400

    Touch up `icewatch --help` output

--- src/icewatch/__init__.py
+++ src/icewatch/__init__.py
@@ -10,16 +10,31 @@ def main():
     parser = argparse.ArgumentParser(
         prog="icewatch",
         description="Command line tool to download ICE detention statistics",
+        formatter_class=argparse.RawDescriptionHelpFormatter,
+        epilog="""Example Usage:
+    icewatch scrape
+    icewatch geocode --input data/ice_facilities_20250704_162107.json
+    icewatch render  --input data/facilities_geocoded_20250704_162107.json""",
     )
 
     subparsers = parser.add_subparsers(
         dest="command",
-        help="available commands",
+        title="commands",
         required=True,
     )
-    subparsers.add_parser("scrape", add_help=False)
-    subparsers.add_parser("geocode", add_help=False)
-    subparsers.add_parser("render", add_help=False)
+    subparsers.add_parser(
+        name="scrape",
+        add_help=False,
+        help="Download ICE detention statistics Excel file",
+    )
+    subparsers.add_parser(
+        name="geocode",
+        add_help=False,
+        help="Geocode facilities JSON using OpenStreetMap Nominatim with caching.",
+    )
+    subparsers.add_parser(
+        name="render", add_help=False, help="Render a static HTML map of facilities."
+    )
 
     args, argv_tail = parser.parse_known_args()
     sys.argv = [
