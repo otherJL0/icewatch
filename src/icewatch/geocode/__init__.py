e5377316b771cf5fac20c99f1de3545d62b7ef7e
Jonathan Lopez <jonathanglopez@gmail.com>
2025-08-13 12:35:49 -0400

    Add mapbox api

--- src/icewatch/geocode/__init__.py
+++ src/icewatch/geocode/__init__.py
@@ -1,4 +1,29 @@
-from icewatch.geocode.nomination import geocode_address
+import logging
+import os
+from functools import partial
+from typing import Callable
+
+import requests
+
+from icewatch.geocode.mapbox import query_mapbox
+from icewatch.geocode.types import Coordinates
+
+# Configure logging
+logging.basicConfig(
+    level=logging.INFO, format="%(asctime)s - %(levelname)s - %(message)s"
+)
+logger = logging.getLogger(__name__)
+
+geocode_address: Callable[[str, requests.Session | None], Coordinates | None]
+
+MAPBOX_ACCESS_TOKEN = os.environ.get("MAPBOX_ACCESS_TOKEN")
+if MAPBOX_ACCESS_TOKEN:
+    geocode_address = partial(query_mapbox, MAPBOX_ACCESS_TOKEN)
+    logger.info("MAPBOX_ACCESS_TOKEN found, using Mapbox api")
+else:
+    from icewatch.geocode.nomination import geocode_address
+
+    logger.warning("No MAPBOX_ACCESS_TOKEN found, falling back to nomination")
 
 
 __all__ = ["geocode_address"]
